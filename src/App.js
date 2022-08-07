74400c0f8448be15e2faedf7eaeee619187296b1
Pulatof <niyozbekpulatov@gmail.com>
2022-08-07 14:53:13 +0500

    first setups completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const valueRef = useRef(null);
+  const [todos, setTodos] = useState([]);
+
+  const addTodos = (e) => {
+    if (valueRef.current.value.trim().length) {
+      console.log(111);
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text: valueRef.current.value,
+          completed: false,
+        },
+      ]);
+    }
+  };
+
+  const removeTodo = (id) => {
+    setTodos(
+      todos.filter((val) => {
+        return val.id !== id;
+      })
+    );
+  };
+
+  const completedTodo = (id) => {
+    todos.filter((val) => {
+      if (val.id !== id) return val;
+
+      return {
+        ...val,
+        completed: !val.completed,
+      };
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>
+        <input type="text" ref={valueRef} placeholder="enter what you do" />
+        <button onClick={addTodos}>Enter</button>
+      </label>
+      <ul>
+        {todos.map((val) => {
+          return (
+            <li key={val.id}>
+              <input
+                type="checkbox"
+                checked={val.checked}
+                onChange={() => completedTodo(val.id)}
+              />
+              <span>{val.text}</span>
+              <span className="delete" onClick={() => removeTodo(val.id)}>
+                &times;
+              </span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
